5232f1e04bae2a0b713e16ad6805e7e655062743
Humming-moon-afk <milian.loechnr@gmail.com>
2026-02-27 16:14:26 +0100

    Klasse excercise erstellt mit basic Konstruktor

---
+++ excercise.java
@@ -0,0 +1,10 @@
+public class excercise {
+    private String name;
+    private int reps;
+    private int gewicht;
+    public excercise(String name, int reps, int gewicht) {
+        this.name = name;
+        this.reps = reps;
+        this.gewicht = gewicht;
+    }
+}
