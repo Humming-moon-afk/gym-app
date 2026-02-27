ed5c7911d7cdb4c8bc9c73f183d6cfbb14193678
Humming-moon-afk <milian.loechner@gmail.com>
2026-02-27 17:01:17 +0100

    erneut try catch dieses mal richtig catch()

--- Main.java
+++ Main.java
@@ -3,12 +3,9 @@ public class Main {
         Exercise Tag1 = new Exercise("Bankdrücken", 50, 20.0);
         System.out.println(Tag1);
         try {
-            Exercise fail = new Exercise("Test", 20, -5);
-        } catch {
-            (IllegalArgumentException e) {
-                System.err.println("Wiederholung oder Gewicht war negativ.");
-                System.err.println("Grund: " + e.getMessage());
-            }
+            Exercise fail = new Exercise("Fail-Test", 20, -1);
+        } catch(IllegalArgumentException e) {
+            System.err.println("Fehler!" + " Grund: " + e.getMessage());
         }
     }
 }
