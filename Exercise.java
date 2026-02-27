c9c2ec7523f4bb24cd9b3b79937247b437742dd9
Humming-moon-afk <milian.loechnr@gmail.com>
2026-02-27 16:16:25 +0100

    excercise auf Exercise geändert(Klassennamen immer groß) + int gewicht auf double geändert

--- Exercise.java
+++ Exercise.java
@@ -1,8 +1,8 @@
-public class excercise {
+public class Exercise {
     private String name;
     private int reps;
-    private int gewicht;
-    public excercise(String name, int reps, int gewicht) {
+    private double gewicht;
+    public excercise(String name, int reps, double gewicht) {
         this.name = name;
         this.reps = reps;
         this.gewicht = gewicht;
