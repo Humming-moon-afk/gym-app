cff2a7287b85b4d18927ceef12feee55ad400c74
Humming-moon-afk <milian.loechnr@gmail.com>
2026-02-27 16:24:07 +0100

    Konsturktor auf richtigen Klassennamen abgeändert(excercise auf Exercise).
    Test im Konstruktor falls gewicht oder reps negativ sind -> throw new IllegalArgumentException(");

--- Exercise.java
+++ Exercise.java
@@ -2,9 +2,12 @@ public class Exercise {
     private String name;
     private int reps;
     private double gewicht;
-    public excercise(String name, int reps, double gewicht) {
+    public Exercise(String name, int reps, double gewicht) {
+        if(gewicht < 0 || reps < 0)  {
+            throw new IllegalArgumentException("Ungültig");
         this.name = name;
         this.reps = reps;
         this.gewicht = gewicht;
+    }
     }
 }
