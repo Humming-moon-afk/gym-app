6568af04f3f6fe67b08a414ae7d2298111d3a743
Humming-moon-afk <milian.loechner@gmail.com>
2026-02-27 16:35:47 +0100

    Setter + Getter

--- Exercise.java
+++ Exercise.java
@@ -10,5 +10,27 @@ public class Exercise {
         this.gewicht = gewicht;
     }
     }
-    hi
+    public String getName() {
+        return name;
+    }
+    public int getReps() {
+        return reps;
+    }
+    public double getGewicht() {
+        return gewicht;
+    }
+    public setName(String name) {
+        this.name = name;
+    }
+    public setReps(int reps) {
+        if(reps < 0) {
+            throw new IllegalArgumentException("Ungültig");
+        }
+        this.reps = reps;
+    }
+    public setGewicht(double gewicht) {
+        if(gewicht < 0) {
+            throw new IllegalArgumentException("Ungültig");
+        }
+    }
 }
